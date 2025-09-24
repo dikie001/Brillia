150733c195bf4e827de5ed689affa42e42e7fd93
dikie001 <omondidickens255@gmail.com>
2025-09-24 17:06:51 +0300

    update MiniStories.tsx

--- src/pages/MiniStories.tsx
+++ src/pages/MiniStories.tsx
@@ -236,7 +236,7 @@ export default function MiniStories() {
       {/* Story Modal */}
       {selectedStoryData && (
         <div
-          className="fixed inset-0 bg-black/80 backdrop-blur-sm z-50 flex items-center justify-center p-4"
+          className="fixed inset-0 bg-black/80  backdrop-blur-sm z-50 flex items-center justify-center p-4"
           onClick={() => setSelectedStory(null)}
         >
           <div
