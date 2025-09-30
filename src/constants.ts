021150ab3f55a8ab257660997f3a505dab8bead4
dikie001 <omondidickens255@gmail.com>
2025-09-30 09:58:43 +0300

    `Update constants in src/constants.ts: add APP_URL and remove trailing whitespace`

--- src/constants.ts
+++ src/constants.ts
@@ -7,16 +7,19 @@ export const STORAGE_KEYS = {
 export const FONT_SIZES = ["small", "medium", "large"] as const;
 
 // Brain TEasers current page
-export const TEASERS_CURRENTPAGE = "brain-teaser-currentPage"
+export const TEASERS_CURRENTPAGE = "brain-teaser-currentPage";
 
 // Mini Stories current page
-export const STORIES_CURRENTPAGE = "mini-stories-currentPage"
+export const STORIES_CURRENTPAGE = "mini-stories-currentPage";
 
 // Wisdom Nuggets current page
-export const WISDOM_CURRENTPAGE = "wisdom-nuggets-currentPage"
+export const WISDOM_CURRENTPAGE = "wisdom-nuggets-currentPage";
 
 // Amazing Facts current page
-export const FACTS_CURRENTPAGE = "amazing-facts-currentPage"
+export const FACTS_CURRENTPAGE = "amazing-facts-currentPage";
 
 // Tongue Twisters current page
-export const TONGUETWISTERS_CURRENTPAGE = "tongue-twisters-currentPage"
+export const TONGUETWISTERS_CURRENTPAGE = "tongue-twisters-currentPage";
+
+// App URL
+export const APP_URL = "https://brillia-six.vercel.app";
