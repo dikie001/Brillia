3727cdd8d47f4893c1e61c879db04fd4b2346756
dikie001 <omondidickens255@gmail.com>
2025-12-06 19:20:28 +0300

    `Removed unnecessary import of React from "react" in Loader.tsx`

--- src/pages/ADMIN/Loader.tsx
+++ src/pages/ADMIN/Loader.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const LoaderPage = () => {
   const [loadingText, setLoadingText] = useState("INITIALIZING_CORE");
